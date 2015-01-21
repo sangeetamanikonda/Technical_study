7c056702b9a6c0e717b13f4ce33f415f9e1604ad
sangeetamanikonda <sangeeta.m89@gmail.com>
2015-01-21 12:19:08 +0530

    update

--- UserSpace/1.lib/main.c
+++ UserSpace/1.lib/main.c
@@ -3,10 +3,11 @@
 
 int main(void)
 {
-  double a=10.34;
-  double b=10.34;
+  double a=2.2;  
+  double b=4.4;
 
-  printf("\n %F\n",get_mean(a,b));
+  //get mean value either using static or shared library
+  printf("\n Result: \n a=%0.2F  b=%0.2F  Mean=%0.2F\n",a,b,get_mean(a,b));
   return 0;
 }
 
